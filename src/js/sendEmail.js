982b214711e29da19b4db10ecacbf86925a39022
Fabio Filho <guedesf140@gmail.com>
2025-05-11 18:34:54 -0300

    code indented with prettier

--- src/js/sendEmail.js
+++ src/js/sendEmail.js
@@ -1,7 +1,7 @@
 function sendEmail() {
-    const subject = encodeURIComponent(document.getElementById('subject').value);
-    const body = encodeURIComponent(document.getElementById('body').value);
-    const mailtoLink = `mailto:liteDevTools@gmail.com?subject=${subject}&body=${body}`
-    
-    window.location.href = mailtoLink;
+  const subject = encodeURIComponent(document.getElementById("subject").value);
+  const body = encodeURIComponent(document.getElementById("body").value);
+  const mailtoLink = `mailto:liteDevTools@gmail.com?subject=${subject}&body=${body}`;
+
+  window.location.href = mailtoLink;
 }
