262424020f3c700b05fcc1e3fbbb1e0697f82e59
Fabio Filho <guedesf140@gmail.com>
2025-06-13 11:51:27 -0300

    made gear switch colors with theme and added placeholder text for future dropdown menu

--- src/js/theme.js
+++ src/js/theme.js
@@ -13,12 +13,19 @@ function switchTheme(theme) {
       document.body.style.setProperty("--text-color", "whitesmoke");
       document.body.style.setProperty("--title-color", "white");
       document.body.style.setProperty("--division-color", "#909090");
+      document
+        .getElementById("configGear")
+        .style.setProperty("filter", "invert(0)");
+      break;
       break;
     case "light":
       document.body.style.setProperty("--bg-color", "#e0f7ff9e");
       document.body.style.setProperty("--text-color", "#555");
       document.body.style.setProperty("--title-color", "black");
       document.body.style.setProperty("--division-color", "white");
+      document
+        .getElementById("configGear")
+        .style.setProperty("filter", "invert(1)");
       break;
   }
 }
