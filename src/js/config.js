fa5db5dc3f6af4de996a78747a522d0abf5cc2e4
Fabio Filho <guedesf140@gmail.com>
2025-06-15 16:09:49 -0300

    added functionality to options menu, made font size switching

---
+++ src/js/config.js
@@ -0,0 +1,18 @@
+const configGear = document.getElementById("configGear");
+const menu = document.getElementById("configDropdown");
+menuOpen = false;
+
+function updateMenu() {
+  if (menuOpen) {
+    menu.style.setProperty("opacity", "1");
+    menu.style.setProperty("transform", "translateY(0)");
+    return;
+  }
+  menu.style.setProperty("opacity", "0");
+  menu.style.setProperty("transform", "translateY(-10px)");
+}
+
+configGear.addEventListener("click", function () {
+  menuOpen = !menuOpen;
+  updateMenu();
+});
---
+++ src/js/font.js
@@ -0,0 +1,43 @@
+const fontChoice = document.getElementById("fontChoice");
+
+const savedFont = localStorage.getItem("font");
+if (savedFont) {
+  switchFont(savedFont);
+  fontChoice.value = savedFont;
+}
+
+function switchFont(font) {
+  sideNavBar = document.getElementById("sideNavbar");
+  switch (font) {
+    case "small":
+      percentage = 70;
+      document.body.style.setProperty("font-size", percentage.toString() + "%");
+      sideNavBar.style.setProperty(
+        "width",
+        ((110 * percentage) / 100).toString() + "px"
+      );
+      break;
+    case "mid":
+      percentage = 100;
+      document.body.style.setProperty("font-size", percentage.toString() + "%");
+      sideNavBar.style.setProperty(
+        "width",
+        ((110 * percentage) / 100).toString() + "px"
+      );
+      break;
+    case "big":
+      percentage = 135;
+      document.body.style.setProperty("font-size", percentage.toString() + "%");
+      sideNavBar.style.setProperty(
+        "width",
+        ((110 * percentage) / 100).toString() + "px"
+      );
+      break;
+  }
+}
+
+fontChoice.addEventListener("input", function () {
+  const selectedFont = fontChoice.value;
+  switchFont(selectedFont);
+  localStorage.setItem("font", selectedFont);
+});
