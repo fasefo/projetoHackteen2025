5356482baf6a42fe1002a31276f464ad2157b88a
Fabio Filho <guedesf140@gmail.com>
2025-06-16 11:11:36 -0300

    fixed bug where you could click at the config menu while closed and made it so that it closes if you click off it

--- src/js/config.js
+++ src/js/config.js
@@ -8,14 +8,23 @@ updateMenu();
 function updateMenu() {
   if (menuOpen) {
     menu.style.setProperty("opacity", "1");
+    menu.style.setProperty("pointer-events", "auto");
     menu.style.setProperty("transform", "translateY(0)");
     return;
   }
   menu.style.setProperty("opacity", "0");
+  menu.style.setProperty("pointer-events", "none");
   menu.style.setProperty("transform", "translateY(-10px)");
 }
 
 configGear.addEventListener("click", function () {
   menuOpen = !menuOpen;
   updateMenu();
+});
+
+document.addEventListener("click", function (event) {
+  if (!menu.contains(event.target) && !configGear.contains(event.target)) {
+    menuOpen = false;
+    updateMenu();
+  }
 });
