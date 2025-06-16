5b10af57f2cd1645bd41b6f30e06029b65282c0d
Fabio Filho <guedesf140@gmail.com>
2025-06-16 12:02:22 -0300

    made grid tools and search feedback

--- src/js/pesquisa.js
+++ src/js/pesquisa.js
@@ -2,11 +2,19 @@ function searchTools() {
   const searchInput = document
     .getElementById("searchInput")
     .value.toLowerCase();
-  const tools = document.querySelectorAll(".tool");
-
+  if (searchInput != "") {
+    document
+      .getElementById("searchFeedback")
+      .style.setProperty("display", "block");
+  } else {
+    document
+      .getElementById("searchFeedback")
+      .style.setProperty("display", "none");
+  }
+  const tools = document.querySelectorAll(".toolWrapper");
+  console.log(tools);
   tools.forEach((tool) => {
     const toolTitle = tool.querySelector("p").textContent.toLowerCase();
-
     if (toolTitle.includes(searchInput)) {
       tool.style.display = "flex";
     } else {
