bf93deb87822f606cea4034baac7747314bd3576
Fabio Filho <guedesf140@gmail.com>
2025-06-22 20:38:08 -0300

    made tools dropdown better

--- src/js/navbarSize.js
+++ src/js/navbarSize.js
@@ -8,7 +8,18 @@ const resizeObserver = new ResizeObserver((entries) => {
     size = (210 / 1980) * width;
     size = Math.max(size, 110);
     navbar.style.setProperty("flex", "0 1 " + size + "px");
+    var content = document.getElementById("toolsContent");
+    content.style.setProperty("left", size + 15 + "px");
   }
 });
+
+function updateNavBar() {
+  const width = document.body.getBoundingClientRect().width;
+  size = (210 / 1980) * width;
+  size = Math.max(size, 110);
+  navbar.style.setProperty("flex", "0 1 " + size + "px");
+  var content = document.getElementById("toolsContent");
+  content.style.setProperty("left", size + 15 + "px");
+}
 
 resizeObserver.observe(body);
---
+++ src/js/toolsDropdown.js
@@ -0,0 +1,25 @@
+toolsOpen = true;
+
+function toggleContent() {
+  var content = document.getElementById("toolsContent");
+  if (toolsOpen) {
+    toolsOpen = false;
+    content.style.setProperty("opacity", "1");
+    content.style.setProperty("pointer-events", "auto");
+  } else {
+    toolsOpen = true;
+    content.style.setProperty("opacity", "0");
+    content.style.setProperty("pointer-events", "none");
+  }
+  updateNavBar();
+}
+
+requestAnimationFrame(() => {
+  document.addEventListener("click", function (event) {
+    var content = document.getElementById("toolsContent");
+    var trigger = document.getElementById("toolsDropdownTrigger");
+    if (!content.contains(event.target) && !trigger.contains(event.target)) {
+      toggleContent();
+    }
+  });
+});
