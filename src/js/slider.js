77328123610344d41a2156a66c108bd474c6e29b
Fabio Filho <guedesf140@gmail.com>
2025-07-03 14:06:26 -0300

    fixed slider resizing issue

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,6 +1,17 @@
 let counter = 1;
 setInterval(() => {
-  document.getElementById('radio' + counter).checked = true;
+  document.getElementById("radio" + counter).checked = true;
   counter++;
   if (counter > 4) counter = 1;
 }, 5000);
+
+const slider = document.querySelector(".slider");
+
+const sliderResizeObserver = new ResizeObserver((entries) => {
+  for (let entry of entries) {
+    const { width, height } = entry.contentRect;
+    slider.style.setProperty("height", `${(width * 350) / 1000}px`);
+  }
+});
+
+sliderResizeObserver.observe(slider);
